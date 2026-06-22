2aa56fd46196a7b41ff724ed9644175a5bba61a5
Usama Hafeez <121376650+UsamaHafeez2351@users.noreply.github.com>
2026-06-22 12:56:01 +0500

    Refactoring

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -11,7 +11,7 @@ class AppRoutes {
   static const signup = '/signup';
   static const home = '/home';
   static const profile = '/profile';
-
+  
   static final routes = [
     GetPage(
       name: splash,
